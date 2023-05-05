c71ad08ff79ac8bb38608de1b5d7fffdd95e547e
Mohan Kumar Kothala <33448410+mohankumar17@users.noreply.github.com>
2023-05-05 17:36:32 +0530

    Update streamlit_app.py
    updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Idly")
-streamlit.text("Dosa")
-streamlit.text("Vada")
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
